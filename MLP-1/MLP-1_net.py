a70565c94e7aee4e88942d42e2a46b2340b2c612
Zouzixing <z124924446@gmail.com>
2022-06-05 16:10:05 +0200

    6.5 update: mlp-Model based on sklearn

--- MLP-1/MLP-1_net.py
+++ MLP-1/MLP-1_net.py
@@ -1,55 +1,66 @@
-#MLP的模型框架结构(1层隐藏层)
+#MLP的模型框架结构(1层隐藏层),sklearn搭建
 #coding: utf-8
 
-import greyImage_generate
-from keras.layers import *
-from keras.models import *
+import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
-from tensorflow.keras.optimizers import Adam
-from model_profiler import model_profiler
-import tensorflow as tf
+import joblib
+from sklearn.model_selection import train_test_split
 from keras_flops import get_flops
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
+#读入数据集并作标准化处理
+def get_datasets():
+    # 每个样本为（2048，）
+    datasets, labels = np.load('./data/database.npy'), np.load('./data/labels.npy')
+    # 数据集划分
+    X_train, X_test, y_train, y_test = train_test_split(datasets, labels, test_size=0.2, random_state=0)
+    y_train, y_test = y_train.flatten(), y_test.flatten()
+    #数据标准化，MLP-API中提到，Multi-layer Perceptron is sensitive to feature scaling
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
+    return X_train, X_test, y_train, y_test
 
-train_segments, train_labels,validation_segments, validation_labels = greyImage_generate.get_tfrecord()
-#由于数据集中各个数据值的差别不大，故不需要进行数据预处理
+def model_train():
+    X_train, X_test, y_train, y_test = get_datasets()
+    #搭建模型，迭代次数设置为100
+    mlp = MLPClassifier(max_iter=100)
+    #网格搜索的方式来大致确定下超参数设置,hidden_layer_size中，元素个数表示隐层层层数，数值表示改层神经元数量，这里是单层神经元，只调个数
+    parameter_space = {
+        'hidden_layer_sizes': [(10,), (20,), (30,),(40,),(50,)],
+        'activation': ['tanh', 'relu'],
+        'solver': ['sgd', 'adam'],
+        'alpha': [0.0001, 0.05],
+        'learning_rate': ['constant', 'adaptive'],
+    }
+    #n-jobs表示使用CPU数，-1为所有可用CPU，cv为交叉验证折数
+    clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+    clf.fit(X_train, y_train)
+    # Best paramete set
+    print('Best parameters found:\n', clf.best_params_)
+    # All results
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+    #模型验证，输出结果报告
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print('Results on the test set:')
+    print(classification_report(y_true, y_pred))
 
-#搭建模型
-model = Sequential()
+    '''
+    flops = get_flops(model,batch_size=32)
+    flops = flops/2
+    print(f"FLOPS: {flops / 10 ** 9:.03} G")
+    '''
+    #保存模型
+    joblib.dump(clf, "mlp.pkl")
+    print("[INFO] Model has been saved !")
 
-model.add(Dense(units=512,activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(units=4,activation='softmax'))
-model.summary()
-adam = Adam(learning_rate=0.0001,beta_1=0.9, beta_2=0.999, epsilon=1e-08,decay=0.0,amsgrad=False)
-model.compile(optimizer=adam,loss='categorical_crossentropy',metrics=['accuracy'])
-
-flops = get_flops(model,batch_size=32)
-flops = flops/2
-print(f"FLOPS: {flops / 10 ** 9:.03} G")
-
-history = model.fit(train_segments,train_labels,epochs=10,batch_size=32,validation_split=0.2,shuffle=True)
-model.evaluate(validation_segments,validation_labels)
-model.save('grey.h5')
-
-
-#训练过程可视化
-fig = plt.figure()#新建一张图
-plt.plot(history.history['accuracy'],label='training acc')
-plt.plot(history.history['val_accuracy'],label='val acc')
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(loc='lower right')
-fig.savefig('1DCNN'+"20000_noise10"+'acc.png')  #视情况修改
-fig = plt.figure()
-plt.plot(history.history['loss'],label='training loss')
-plt.plot(history.history['val_loss'], label='val loss')
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(loc='upper right')
-fig.savefig('1DCNN'+"20000_noise10"+'loss.png')  #视情况修改
-
+if __name__ == "__main__":
+    model_train()
