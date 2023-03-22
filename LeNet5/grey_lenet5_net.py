4cc722dd5eb1cdd789aac442ec5542c4c5ddfb09
Administrator <z124924446@gmail.com>
2023-03-22 19:30:41 +0800

    调整代码结构,加入Dropout防止过拟合，调整部分参数

--- LeNet5/grey_lenet5_net.py
+++ LeNet5/grey_lenet5_net.py
@@ -30,14 +30,20 @@ model = Sequential()
 model.add(Conv2D(filters=32,kernel_size=5,strides=1,activation='relu',padding='same',input_shape=(64,64,1)))
 #Pooling layer 1
 model.add(MaxPooling2D(pool_size=2,strides=2))
+#Dropout1
+model.add(Dropout(0.20))
 #Layer 2
 #Conv Layer 2
 model.add(Conv2D(filters=64,kernel_size=3,strides=1,activation='relu',padding='same',input_shape=(32,32,32)))
 #Pooling layer 2
 model.add(MaxPooling2D(pool_size=2,strides=2))
+#Dropout2
+model.add(Dropout(0.20))
 #Layer 3
 #Conv Layer 3
 model.add(Conv2D(filters=128,kernel_size=3,strides=1,activation='relu',padding='same',input_shape=(16,16,64)))
+#Dropout3
+model.add(Dropout(0.20))
 #Pooling layer 3
 model.add(MaxPooling2D(pool_size=2,strides=2))
 #Layer 4
@@ -45,11 +51,13 @@ model.add(MaxPooling2D(pool_size=2,strides=2))
 model.add(Conv2D(filters=256,kernel_size=3,strides=1,activation='relu',padding='same',input_shape=(8,8,128)))
 #Pooling layer 4
 model.add(MaxPooling2D(pool_size=2,strides=2))
+#Dropout4
+model.add(Dropout(0.20))
 #Flatten
 model.add(Flatten())
 #Layer 5
 #Fully connected layer 1
-model.add(Dense(units=2560,activation='relu'))
+model.add(Dense(units=84,activation='relu'))
 #model.add(Dense(units=768,activation='relu'))
 #Layer 6
 #output Layer,标签为4类，所以units设定为4
@@ -58,28 +66,32 @@ model.summary()
 adam = Adam(learning_rate=0.00001,beta_1=0.9, beta_2=0.999, epsilon=1e-08,decay=0.0,amsgrad=False)
 model.compile(optimizer=adam,loss='categorical_crossentropy',metrics=['accuracy'])
 
-flops = get_flops(model,batch_size=16)
-flops = flops/2
-print(f"FLOPS: {flops / 10 ** 9:.03} G")
+# flops = get_flops(model,batch_size=16)
+# flops = flops/2
+# print(f"FLOPS: {flops / 10 ** 9:.03} G")
 
-history = model.fit(X_train,y_train,epochs=160,batch_size=16,validation_split=0.2)
+history = model.fit(X_train,y_train,epochs=200,batch_size=32,validation_split=0.2)
 model.evaluate(X_test,y_test)
-model.save('grey.h5')
+model.save('LeNet5.h5')
 
 #训练过程可视化
-fig = plt.figure()#新建一张图
+fig = plt.figure()
+plt.xticks(fontproperties = 'Times New Roman', size = 14)
+plt.yticks(fontproperties = 'Times New Roman', size = 14)
 plt.plot(history.history['accuracy'],label='training acc')
 plt.plot(history.history['val_accuracy'],label='val acc')
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(loc='lower right')
+plt.title('model accuracy',fontdict={'family' : 'Times New Roman', 'size'   : 16})
+plt.ylabel('accuracy',fontdict={'family':'Times New Roman', 'size':16})
+plt.xlabel('epoch',fontdict={'family':'Times New Roman', 'size':16})
+plt.legend(loc='lower right',prop={'family' : 'Times New Roman', 'size'   : 16})
 fig.savefig('LeNet5_'+'acc.png')
 fig = plt.figure()
+plt.xticks(fontproperties = 'Times New Roman', size = 14)
+plt.yticks(fontproperties = 'Times New Roman', size = 14)
 plt.plot(history.history['loss'],label='training loss')
 plt.plot(history.history['val_loss'], label='val loss')
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(loc='upper right')
+plt.title('model loss',fontdict={'family' : 'Times New Roman', 'size'   : 16})
+plt.ylabel('loss',fontdict={'family':'Times New Roman', 'size':16})
+plt.xlabel('epoch',fontdict={'family':'Times New Roman', 'size':16})
+plt.legend(loc='upper right',prop={'family' : 'Times New Roman', 'size'   : 16})
 fig.savefig('LeNet5_'+'loss.png')
